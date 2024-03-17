3e196de94cd147aace7f4a4188e27ac48d4798b5
Leiner2024 <136385391+Leiner2024@users.noreply.github.com>
2024-03-17 15:53:01 +0100

    Update Ej4.py

--- Ej4.py
+++ Ej4.py
@@ -2,4 +2,4 @@ l = ["Super", "Hiper", "Mini", "Maxi"]
 d = ["Women", "bole", "Mouse", "Boom"]
 c = list(zip(l, d))
 
-print("La unión de las {} y {} es {}".format(l,d,c))
+print("La unión de las listas {} y {} es {}".format(l,d,c))
